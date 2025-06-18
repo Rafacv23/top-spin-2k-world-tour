d6457aaa15330477aea20b6ec1e0ea99516eda9d
Rafa <rafacv23@gmail.com>
2025-06-18 22:09:05 +0200

    Add features and contact sections; update footer styling and introduce table components

---
+++ src/components/landing/about.tsx
@@ -0,0 +1,113 @@
+import { buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card"
+import Link from "next/link"
+
+export default function About() {
+  return (
+    <section id="about" className="h-screen pt-20 flex flex-col gap-4">
+      <div className="flex items-center justify-center bg-background">
+        <span className="absolute mx-auto py-4 flex border w-fit bg-gradient-to-r blur-xl from-white to-primary bg-clip-text text-4xl box-content font-extrabold text-transparent text-center select-none">
+          Help us to Grow
+        </span>
+        <h2 className="relative top-0 w-fit h-auto py-4 justify-center flex bg-gradient-to-r items-center from-white to-primary  bg-clip-text text-4xl font-extrabold text-transparent text-center select-auto">
+          Help us to Grow
+        </h2>
+      </div>
+      <p className="text-2xl">
+        Help us to grow our platform. Invite your friends, share the community,
+        and get ready to climb the ranks.
+      </p>
+      <footer className="mt-4">
+        <ul className="flex items-center justify-center gap-4">
+          <li>
+            <Card>
+              <CardHeader>
+                <CardTitle>Reddit</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  Rankings based on performance and statistics for all players.
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href={"/register"}
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  Join
+                </Link>
+              </CardFooter>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <CardHeader>
+                <CardTitle>Discord</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  Rankings based on performance and statistics for all players.
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href={"/register"}
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  Join
+                </Link>
+              </CardFooter>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <CardHeader>
+                <CardTitle>Instant Gaming</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  Rankings based on performance and statistics for all players.
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href={"/register"}
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  Join
+                </Link>
+              </CardFooter>
+            </Card>
+          </li>
+          <li>
+            <Card>
+              <CardHeader>
+                <CardTitle>Invite</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  Rankings based on performance and statistics for all players.
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href={"/register"}
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  Join
+                </Link>
+              </CardFooter>
+            </Card>
+          </li>
+        </ul>
+      </footer>
+    </section>
+  )
+}
--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -1,89 +1,63 @@
-import { buttonVariants } from "@/components/ui/button"
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
   CardDescription,
-  CardFooter,
 } from "@/components/ui/card"
-import Link from "next/link"
 
-export default function Contact() {
+export default function Features() {
   return (
-    <section id="contact" className="py-20 flex flex-col gap-4">
+    <section id="features" className="h-screen pt-20 flex flex-col gap-4">
       <div className="flex items-center justify-center bg-background">
         <span className="absolute mx-auto py-4 flex border w-fit bg-gradient-to-r blur-xl from-white to-primary bg-clip-text text-4xl box-content font-extrabold text-transparent text-center select-none">
-          Contact
+          Weekly Tournaments & Live Rankings
         </span>
         <h2 className="relative top-0 w-fit h-auto py-4 justify-center flex bg-gradient-to-r items-center from-white to-primary  bg-clip-text text-4xl font-extrabold text-transparent text-center select-auto">
-          Contact{" "}
+          Weekly Tournaments & Live Rankings
         </h2>
       </div>
       <p className="text-2xl">
-        If you have any questions, suggestions, or need assistance, feel free to
-        reach out to us via our social media channels.
+        Major title events, weekly and monthly tournaments with points. Live
+        rankings based on performance and statistics for all players.
       </p>
       <footer className="mt-4">
         <ul className="flex items-center justify-center gap-4">
           <li>
             <Card>
               <CardHeader>
-                <CardTitle>Mail</CardTitle>
+                <CardTitle>Live Rankings</CardTitle>
               </CardHeader>
               <CardContent>
                 <CardDescription>
                   Rankings based on performance and statistics for all players.
                 </CardDescription>
               </CardContent>
-              <CardFooter>
-                <Link
-                  href={"/register"}
-                  className={buttonVariants({ variant: "outline" })}
-                >
-                  Join
-                </Link>
-              </CardFooter>
             </Card>
           </li>
           <li>
             <Card>
               <CardHeader>
-                <CardTitle>Discord</CardTitle>
+                <CardTitle>Weekly Tournaments</CardTitle>
               </CardHeader>
               <CardContent>
                 <CardDescription>
-                  Rankings based on performance and statistics for all players.
+                  Weekly and monthly tournaments with ranking points for all the
+                  players.
                 </CardDescription>
               </CardContent>
-              <CardFooter>
-                <Link
-                  href={"/register"}
-                  className={buttonVariants({ variant: "outline" })}
-                >
-                  Join
-                </Link>
-              </CardFooter>
             </Card>
           </li>
           <li>
             <Card>
               <CardHeader>
-                <CardTitle>About</CardTitle>
+                <CardTitle>Major Titles</CardTitle>
               </CardHeader>
               <CardContent>
                 <CardDescription>
-                  Rankings based on performance and statistics for all players.
+                  Major title events, with extra points and more prestige.
                 </CardDescription>
               </CardContent>
-              <CardFooter>
-                <Link
-                  href={"/register"}
-                  className={buttonVariants({ variant: "outline" })}
-                >
-                  Join
-                </Link>
-              </CardFooter>
             </Card>
           </li>
         </ul>
