d9cbbc453d4b8b5dc5427c6f7f55a36b99b97104
Rafa <rafacv23@gmail.com>
2025-06-18 22:21:32 +0200

    Add rankings page and container component; implement player ranking table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/container"
 import About from "@/components/landing/about"
 import Contact from "@/components/landing/contact"
 import Features from "@/components/landing/features"
@@ -5,13 +6,11 @@ import Hero from "@/components/landing/hero"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Hero />
-        <Features />
-        <About />
-        <Contact />
-      </main>
-    </div>
+    <Container>
+      <Hero />
+      <Features />
+      <About />
+      <Contact />
+    </Container>
   )
 }
---
+++ src/app/rankings/page.tsx
@@ -0,0 +1,49 @@
+import Container from "@/components/container"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import players from "@/lib/mocks/players.json"
+
+export default function RankingsPage() {
+  return (
+    <Container>
+      <header className="space-y-4">
+        <h1 className="text-4xl font-bold">
+          Live Top Spin 2k World Tour Rankings
+        </h1>
+        <p className="text-lg">The rankings are updated every day.</p>
+      </header>
+      <div className="bg-card/30 backdrop-blur-2xl p-4 w-full rounded-lg border border-primary/10">
+        <Table>
+          <TableCaption>Full Top Spin 2k World Tour Rankings</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Rank</TableHead>
+              <TableHead>Player</TableHead>
+              <TableHead>Country</TableHead>
+              <TableHead>Tournaments</TableHead>
+              <TableHead className="text-right">Points</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {players.map((player, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell>{player.name}</TableCell>
+                <TableCell>{player.country}</TableCell>
+                <TableCell>{player.tournaments}</TableCell>
+                <TableCell className="text-right">{player.points}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Container>
+  )
+}
