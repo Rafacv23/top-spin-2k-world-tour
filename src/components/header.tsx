201aef99ce7d8074a950a2b73789c248453f8d0c
Rafa <rafacv23@gmail.com>
2025-06-19 14:21:00 +0200

    feat(header): update header background opacity for improved visibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <header className="flex items-center justify-between p-2 bg-card z-50 fixed top-4 left-0 right-0 border backdrop-blur-lg rounded-lg mt-4">
+    <header className="flex items-center justify-between p-2 bg-card/60 z-50 fixed top-4 left-0 right-0 border backdrop-blur-lg rounded-lg mt-4">
       <Link href={"/"} title="Top Spin 2k World Tour">
         <img src="/image-bg.png" alt="logo" width={60} className="rounded-lg" />
       </Link>
